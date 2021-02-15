a21e26f1bc9af4ca18d09848408efff18d5b3dcd
joyner6 <73307307+joyner6@users.noreply.github.com>
2021-02-15 23:49:01 +0100

    Delete ecormernce directory

--- ecormernce/shop/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-from .models import Shipping
-
-
-class CheckOutForm(forms.ModelForm):
-
-    class Meta:
-        model = Shipping
-        fields = ['Address','phone_no','zip_code','city','state','country','billing_address','shipping_address']
